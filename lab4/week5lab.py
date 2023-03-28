cbb52165cace9983cde9ef6e271772bee062a919
Bsubs <56755990+Bsubs@users.noreply.github.com>
2023-03-28 23:57:07 +0800

    Finished thresholding

--- lab4/week5lab.py
+++ lab4/week5lab.py
@@ -35,21 +35,25 @@ class ImageGUI:
         self.filtered_label.grid(column=3, row=0, padx=5, pady=5)
 
         # Create detect edge button
-        self.detect_edge = tk.Button(self.border, text="Detect Edge")
+        self.detect_edge = tk.Button(self.border, text="Detect Edge", command=self.detect_edges)
         self.detect_edge.grid(column=1, row = 1, padx=5, pady=5)
 
         # Create edge detection drop down menu
-        variable = tk.StringVar(self.border)
-        variable.set("one") # default value
-        self.algo_select = tk.OptionMenu(self.border, variable, "one", "two", "three")
+        self.algo = tk.StringVar(self.border)
+        self.algo.set("canny") # default value
+        self.algo_select = tk.OptionMenu(self.border, self.algo, "prewitt", "sobel", "canny")
         self.algo_select.grid(column=1, row=2, padx=5, pady=5)
 
-        # Create edge detection slider
-        self.edge_detection_slider_label = tk.Label(self.border, text="Threshold for edge detection")
-        self.edge_detection_slider_label.grid(column=3, row=1, padx=5, pady=5)
-        self.edge_detection_slider = tk.Scale(self.border, from_=0, to=200, orient=tk.HORIZONTAL)
-        self.edge_detection_slider.grid(column=3, row=2, padx=5, pady=5)
-        
+        # Create edge detection slider widget
+        self.edge_detection_slider = tk.Scale(self.border, label='Threshold for edge detection', from_=0, to=255, orient=tk.HORIZONTAL, length=255, showvalue=0, command=self.detect_edges)
+        self.edge_detection_slider.set('127')
+        self.edge_detection_slider.grid(column=3, row=1, padx=5, pady=5) 
+
+        # Create circle detection slider widget
+        self.circle_detection_slider = tk.Scale(self.border, label='Threshold for circle detection', from_=0, to=255, orient=tk.HORIZONTAL, length=255, showvalue=0)
+        self.circle_detection_slider.set('127')
+        self.circle_detection_slider.grid(column=3, row=2, padx=5, pady=5) 
+
 
 
 
@@ -59,8 +63,7 @@ class ImageGUI:
 
         # Load the chosen image using PIL
         self.original_image = Image.open(file_path)
-        grayscale_image = self.original_image.convert('L')
-        np_image = np.array(grayscale_image)
+        np_image = np.array(self.original_image)
 
         # Convert the equalized image back to PIL format
         pil_image = Image.fromarray(np_image)
@@ -80,6 +83,42 @@ class ImageGUI:
         photo = ImageTk.PhotoImage(pil_image)
         self.image_label.configure(image=photo)
         self.image_label.image = photo
+
+    def detect_edges(self, _event=None):
+        # Convert the original image to grayscale
+        grayscale_image = self.original_image.convert('L')
+        np_image = np.array(grayscale_image)
+
+        # Get the threshold value from slider widget (default = 127)
+        thresh_val = self.edge_detection_slider.get()
+
+        ret, threshold_img = cv2.threshold(np_image, thresh_val, 255, cv2.THRESH_BINARY)
+
+        if self.algo.get() == 'canny':
+            edge_detected = cv2.Canny(image=threshold_img, threshold1=100, threshold2=200)
+        elif self.algo.get() == 'sobel':
+            grad_x = cv2.Sobel(threshold_img, cv2.CV_64F, 1, 0)
+            grad_y = cv2.Sobel(threshold_img, cv2.CV_64F, 0, 1)
+            edge_detected = np.sqrt(grad_x**2 + grad_y**2)
+            
+        # Convert the equalized image back to PIL format
+        pil_image = Image.fromarray(edge_detected)
+
+        # Resize the image to fit in the label
+        width, height = pil_image.size
+        max_size = self.maxsize
+        if width > height:
+            new_width = max_size
+            new_height = int(height * (max_size / width))
+        else:
+            new_width = int(width * (max_size / height))
+            new_height = max_size
+        pil_image = pil_image.resize((new_width, new_height))
+
+        # Convert the image to Tkinter format and display it on the right side
+        photo = ImageTk.PhotoImage(pil_image)
+        self.filtered_label.configure(image=photo)
+        self.filtered_label.image = photo
         
             
 if __name__ == "__main__":
